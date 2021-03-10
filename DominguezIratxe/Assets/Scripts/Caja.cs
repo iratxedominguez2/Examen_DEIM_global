f1b999439fddc68c2c319766655669f1df8e14eb
iratxedominguez2 <domingueziratxe7@gmail.com>
2021-03-10 19:17:56 +0100

    Interactividad.
    La caja se mueve en X y Z, y su movimiento está restringido.

--- DominguezIratxe/Assets/Scripts/Caja.cs
+++ DominguezIratxe/Assets/Scripts/Caja.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Caja : MonoBehaviour
 {
+
+    
+    public float speed=10f;
+
+    //Punto de destino
+    //private Vector3 moveInput;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed=10f;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        MoverCaja();
+
+
+    }
+
+
+
+
+    void MoverCaja()
+    {
+        float posX = transform.position.x;
+        float posZ = transform.position.z;
+        float desplZ = Input.GetAxis("Vertical");
+        float desplX = Input.GetAxis("Horizontal");
+
+        if (posX < 18f && posX > -18f || posX < -18f && desplX > 0 || posX > 18f && desplX < 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+        }
+
+        if (posZ < 18f && posZ > -18f || posZ < -18f && desplZ > 0 || posZ > 18f && desplZ < 0)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * desplZ);
+        }
     }
 }
