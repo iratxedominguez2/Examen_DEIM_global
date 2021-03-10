f4d84ba3ea5b0bd5fc636431d56030d3b807e3fb
iratxedominguez2 <domingueziratxe7@gmail.com>
2021-03-10 19:19:20 +0100

    Seguimiento cámara.
    La cámara sigue a la caja.

---
+++ DominguezIratxe/Assets/Scripts/Camara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public GameObject caja;
+    private Vector3 desplazamiento;
+    // Start is called before the first frame update
+    void Start()
+    {
+        desplazamiento = transform.position - caja.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = caja.transform.position + desplazamiento;
+
+    }
+}
